135c6eb7bd0fcc8bf701abb9d32589fde07b0ab6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-06-07 15:28:27 +0200

    Update gauntlet and anchors to test Lynx 1.5.0

--- cutechess/gauntlet.ps1
+++ cutechess/gauntlet.ps1
@@ -1,17 +1,17 @@
 &"cutechess-1.3.1-win64/cutechess-cli.exe" `
-    -engine conf="Lynx 1.3.0" `
-    -engine conf="Mess 0.3.0" `
-    -engine conf="Odonata 0.7.0" `
-    -engine conf="Zahak 5.0" `
-    -engine conf="Akimbo 0.4.0" `
-    -engine conf="Stash 21.0" `
-    -engine conf="Polaris 1.5.0" `
-    -engine conf="Pedantic 0.3.0" `
-    -engine conf="MadChess 3.1" `
-    -engine conf="Weiss 0.8" `
-    -engine conf="Princhess 0.11.0" `
-    -engine conf="Luna 2.0.0" `
-    -engine conf="Leorik 2.0" `
+    -engine conf="Lynx 1.5.0" `
+    -engine conf="Stash 25.0" `
+    -engine conf="Simbelmyne 1.7.0" `
+    -engine conf="Weiss 1.0" `
+    -engine conf="Pedantic 0.4.1" `
+    -engine conf="Polaris 1.6.1" `
+    -engine conf="Altair 2.0.0" `
+    -engine conf="Princhess 0.13.0" `
+    -engine conf="Akimbo 0.4.1" `
+    -engine conf="Inanis 1.2.1" `
+    -engine conf="Leorik 2.4" `
+    -engine conf="Zahak 6.2" `
+    -engine conf="Odonata 0.8.0" `
     -each `
         tc=inf/40+0.4 `
         option.Hash=256 `
@@ -21,7 +21,7 @@
     -concurrency 8 `
     -outcomeinterval 10 -ratinginterval 10 -resultformat wide2 `
     -recover `
-    -pgnout "matches\gautnet-x.y.z-ltc.pgn"
+    -pgnout "matches\gautlet-1.5.0-ltc.pgn"
 | tee last-gauntlet.txt
 
     # " -event "Gauntlet $((Get-Date).ToString('yyyy-MM-dd_HH-mm'))" `
