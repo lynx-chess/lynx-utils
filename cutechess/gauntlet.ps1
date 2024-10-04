28f456300aefe875c8cc707c04d1d031dab1238f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-10-05 00:46:39 +0200

    Update for Lynx v1.7.0

--- cutechess/gauntlet.ps1
+++ cutechess/gauntlet.ps1
@@ -1,17 +1,16 @@
 &"cutechess-1.3.1-win64/cutechess-cli.exe" `
-    -engine conf="Lynx 1.5.0" `
-    -engine conf="Stash 25.0" `
-    -engine conf="Simbelmyne 1.7.0" `
-    -engine conf="Weiss 1.0" `
-    -engine conf="Pedantic 0.4.1" `
-    -engine conf="Polaris 1.6.1" `
-    -engine conf="Altair 2.0.0" `
-    -engine conf="Princhess 0.13.0" `
-    -engine conf="Akimbo 0.4.1" `
-    -engine conf="Inanis 1.2.1" `
-    -engine conf="Leorik 2.4" `
-    -engine conf="Zahak 6.2" `
-    -engine conf="Odonata 0.8.0" `
+    -engine conf="Lynx 1.7.0" `
+    -engine conf="Pedantic 1.0.0" `
+    -engine conf="Weiss 1.3" `
+    -engine conf="Clarity 3.0.0" `
+    -engine conf="Alexandria 2.3" `
+    -engine conf="Midnight 7" `
+    -engine conf="Stash 28.0" `
+    -engine conf="Simbelmyne 1.9.0" `
+    -engine conf="Lambergar 0.6.0" `
+    -engine conf="Winter 0.8" `
+    -engine conf="Altair 4.0.0" `
+    -engine conf="Akimbo 0.5.0" `
     -each `
         tc=inf/40+0.4 `
         option.Hash=256 `
@@ -21,10 +20,12 @@
     -concurrency 8 `
     -outcomeinterval 10 -ratinginterval 10 -resultformat wide2 `
     -recover `
-    -pgnout "matches\gautlet-1.5.0-ltc.pgn"
+    -pgnout "matches\gauntlet-1.7.0-ltc.pgn" `
 | tee last-gauntlet.txt
+
+    # -draw movenumber=30 movecount=12 score=15 `
+    # -resign movecount=12 score=600 twosided=true `
 
     # " -event "Gauntlet $((Get-Date).ToString('yyyy-MM-dd_HH-mm'))" `
 
-    # -draw movenumber=30 movecount=12 score=15 `
-    # -resign movecount=12 score=600 twosided=true
+	
--- cutechess/progress.ps1
+++ cutechess/progress.ps1
@@ -1,20 +1,21 @@
 &"cutechess-1.3.1-win64/cutechess-cli.exe" `
-    -engine conf="Lynx 2099 - main" `
-    -engine conf="Cosette 5.1" `
+    -engine conf="Lynx 1.7.0" `
+    -engine conf="Lynx 1.6.0" `
     -each `
         tc=inf/40+0.4 `
         option.Hash=256 `
-    -games 2 -rounds 5000 -repeat 1 -maxmoves 400 -srand 1692649580 `
+    -games 2 -rounds 10000 -repeat 1 -maxmoves 400 -srand 1692649580 `
     -openings file="books\8moves_v3.epd" format=epd order=random `
     -tournament "gauntlet" -seeds 1 `
     -concurrency 8 `
     -outcomeinterval 10 `
     -ratinginterval 10 `
     -recover `
-    -pgnout "matches\progress-x.y.z.pgn"
+    -pgnout "matches\progress-1.7.0.pgn" `
 | tee last-progress.txt
+    # -draw movenumber=30 movecount=12 score=15 `
+    # -resign movecount=12 score=600 twosided=true
 
     # " -event "Gauntlet $((Get-Date).ToString('yyyy-MM-dd_HH-mm'))" `
     # -recover -srand 6730291 `
-    # -draw movenumber=30 movecount=12 score=15 `
-    # -resign movecount=12 score=600 twosided=true
+    #	ponder `
