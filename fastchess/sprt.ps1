f8ea25295f95129ce2cf4ca784fbcac31f1db263
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-07-09 14:02:48 +0200

    Update fastchess stuff

--- fastchess/sprt.ps1
+++ fastchess/sprt.ps1
@@ -1,8 +1,8 @@
-&"fastchess-05da0df\fastchess-windows-latest.exe" `
-    -engine dir="C:/Users/edcaceres/Downloads/Lynx-1.9.1-win-x64/" name="dev" `
-    -engine dir="C:/Users/edcaceres/Downloads/Lynx-1.9.1-win-x64/" name="master" `
+&"fastchess-2a80921\fastchess-windows-latest.exe" `
+    -engine dir="engines/Lynx-search-history-pruning-newformula-and-noisy-6598-win-x64" name="dev" `
+    -engine dir="engines/Lynx-main-6592-win-x64" name="main" `
     -each `
-        tc=4+0.04 `
+        tc=8+0.08 `
         option.Hash=32 `
         option.Threads=1 `
         proto=uci `
@@ -14,9 +14,9 @@
     -sprt elo0=0 elo1=3 alpha=0.05 beta=0.1 model=logistic `
     -openings file="../cutechess/books/UHO_XXL_+0.90_+1.19.epd" format=epd order=random `
     -maxmoves 400 -srand 1692649580 -ratinginterval 10 `
-    -pgnout "matches\sprt-2582.pgn" `
+    -pgnout "matches\sprt-6596.pgn" `
     -recover `
-    -log file=fastchess-errors.log level=warn `
+    -log file=fastchess-logs.log level=warn `
 # | tee last-sprt.txt
 
 
