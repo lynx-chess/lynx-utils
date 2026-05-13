e850222000fbdb7d3429662bc68f41a666f86314
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-05-13 03:11:48 +0200

    Remove Persy cause lack of support for threads

--- cutechess/round-robin.sh
+++ cutechess/round-robin.sh
@@ -1,6 +1,5 @@
 ./cutechess-cli-1.3.1-linux64/cutechess-ob \
     -engine cmd="engines/Lynx-d220df4e" name="Lynx-d220df4e" \
-    -engine cmd="engines/Perseus-2d07444" name="Perseus-2d07444" \
     -engine cmd="engines/Potential-127e8ba9" name="Potential-127e8ba9" \
     -engine cmd="engines/sirius-3bedea6" name="Sirius-3bedea6" \
     -engine cmd="engines/stash-153b1e4" name="Stash-153b1e4" \
@@ -11,7 +10,7 @@
         tc=inf/40+0.4 \
         option.Hash=1024 \
         option.Threads=16 \
-    -games 2 -rounds 100 -repeat 1 -maxmoves 400 -srand 1692649580 \
+    -games 2 -rounds 300 -repeat 1 -maxmoves 400 -srand 1692649580 \
     -openings file="books/UHO_Lichess_4852_v1.epd" format=epd order=random \
         -tournament "round-robin" \
     -concurrency 1 \
