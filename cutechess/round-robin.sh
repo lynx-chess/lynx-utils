6e15362fe5b47541dfff4f7e41060aefe779c297
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-05-13 03:23:05 +0200

    Reduce the number of rounds

--- cutechess/round-robin.sh
+++ cutechess/round-robin.sh
@@ -11,7 +11,7 @@
         tc=inf/40+0.4 \
         option.Hash=1024 \
         option.Threads=16 \
-    -games 2 -rounds 100 -repeat 1 -maxmoves 400 -srand 1692649580 \
+    -games 2 -rounds 10 -repeat 1 -maxmoves 400 -srand 1692649580 \
     -openings file="books/UHO_Lichess_4852_v1.epd" format=epd order=random \
         -tournament "round-robin" \
     -concurrency 1 \
