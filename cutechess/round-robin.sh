77a2910585deb9b3a08f5d409b47df998d14b4d6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-05-13 03:22:07 +0200

    Add CW

--- cutechess/round-robin.sh
+++ cutechess/round-robin.sh
@@ -5,12 +5,13 @@
     -engine cmd="engines/stash-153b1e4" name="Stash-153b1e4" \
     -engine cmd="engines/weiss-95b0951" name="Weiss-95b0951" \
     -engine cmd="engines/stockfish-classical" name="Stockfish-classical" \
+    -engine cmd="engines/clockwork-5bc8f0c" name="clockwork-5bc8f0c" \
     -each \
         proto=uci \
         tc=inf/40+0.4 \
         option.Hash=1024 \
         option.Threads=16 \
-    -games 2 -rounds 150 -repeat 1 -maxmoves 400 -srand 1692649580 \
+    -games 2 -rounds 100 -repeat 1 -maxmoves 400 -srand 1692649580 \
     -openings file="books/UHO_Lichess_4852_v1.epd" format=epd order=random \
         -tournament "round-robin" \
     -concurrency 1 \
