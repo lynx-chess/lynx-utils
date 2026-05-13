06ac8b714e47f781a4a3bda7d4b670d9af1756b2
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-05-13 03:07:01 +0200

    Add names to the engines

--- cutechess/round-robin.sh
+++ cutechess/round-robin.sh
@@ -1,17 +1,17 @@
 ./cutechess-cli-1.3.1-linux64/cutechess-ob \
-    -engine cmd="engines/Lynx-d220df4e" \
-    -engine cmd="engines/Perseus-2d07444" \
-    -engine cmd="engines/Potential-127e8ba9" \
-    -engine cmd="engines/sirius-3bedea6" \
-    -engine cmd="engines/stash-153b1e4" \
-    -engine cmd="engines/weiss-95b0951" \
-    -engine cmd="engines/stockfish-classical" \
+    -engine cmd="engines/Lynx-d220df4e" name="Lynx-d220df4e" \
+    -engine cmd="engines/Perseus-2d07444" name="Perseus-2d07444" \
+    -engine cmd="engines/Potential-127e8ba9" name="Potential-127e8ba9" \
+    -engine cmd="engines/sirius-3bedea6" name="Sirius-3bedea6" \
+    -engine cmd="engines/stash-153b1e4" name="Stash-153b1e4" \
+    -engine cmd="engines/weiss-95b0951" name="Weiss-95b0951" \
+    -engine cmd="engines/stockfish-classical" name="Stockfish-classical" \
     -each \
         proto=uci \
         tc=inf/40+0.4 \
         option.Hash=1024 \
         option.Threads=16 \
-    -games 2 -rounds 1000 -repeat 1 -maxmoves 400 -srand 1692649580 \
+    -games 2 -rounds 100 -repeat 1 -maxmoves 400 -srand 1692649580 \
     -openings file="books/UHO_Lichess_4852_v1.epd" format=epd order=random \
         -tournament "round-robin" \
     -concurrency 1 \
